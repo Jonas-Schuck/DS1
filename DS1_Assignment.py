6bfcef155ecb699758f00be3efceff8c67876152
Josch9 <Jonas_Schuck@hotmail.de>
2022-07-11 21:12:31 +0200

    Create DS1_Assignment.py

---
+++ DS1_Assignment.py
@@ -0,0 +1,26 @@
+import numpy as np
+import pandas as pd
+# you may change the path of the dataset
+data_input = pd.read_csv('C:/Users/User/Dropbox/S2/DS1/Progaufgabe/Datensatz_Diabetes/diabetes_data_upload.csv', header=0)
+
+data_processed = data_input.copy()
+for i in data_input:
+    for j in range(0, 520):
+        if data_input[i][j] == "Yes":
+            data_processed[i][j] = bool(1)
+        if data_input[i][j] == "No":
+            data_processed[i][j] = bool(0)
+        if data_input[i][j] == "Positive":
+            data_processed[i][j] = bool(1)
+        if data_input[i][j] == "Negative":
+            data_processed[i][j] = bool(0)
+        if data_input[i][j] == "Male":
+            data_processed[i][j] = bool(1)
+        if data_input[i][j] == "Female":
+            data_processed[i][j] = bool(0)
+
+data_rules = data_processed.copy()
+data_rules = data_rules.iloc[:, 1:]
+
+
+
